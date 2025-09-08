1aa509a2b11efb2f422c26bbf7132f39f11c04fe
viccic <victsalapati@gmail.com>
2025-09-08 21:36:09 +0200

    changes of the libraries

--- main_Voxel_Grid_Case_1rst_Approach.py
+++ main_Voxel_Grid_Case_1rst_Approach.py
@@ -62,7 +62,7 @@ def main():
 
     # VOXELS CASE
     time_recorder = []
-    voxel_sizes = [0.03, 0.05, 0.07, 0.1, 0.3, 0.5]
+    voxel_sizes = [0.5]
     branches_xyz_file_path = os.path.join(output_path, "Branches.xyz")
     leaves_xyz_file_path = os.path.join(output_path, "Leaves.xyz")
 
